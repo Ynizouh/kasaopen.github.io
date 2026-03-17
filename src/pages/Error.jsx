adc263fb9905694d4f2fcbe4599d3074020852bd
Ynizouh <77572689+Ynizouh@users.noreply.github.com>
2026-03-17 15:44:55 +0100

    feat: Implement 404 error page with responsive styling and add home page banner and gallery styles.

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -6,7 +6,8 @@ function Error() {
         <div className="error">
             <h1 className="error__number">404</h1>
             <p className="error__message">
-                Oups! La page que vous demandez n'existe pas.
+                Oups! La page que <br className="error__br" /> 
+                vous demandez n'existe pas.
             </p>
             <Link to="/" className="error__link">
                 Retourner sur la page d'accueil
